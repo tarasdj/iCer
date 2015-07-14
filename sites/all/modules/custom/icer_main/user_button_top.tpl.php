2e55913dd4c2bda15ec852254bbb8f02d262cd8a
GermaniukT <tarasdj@hotmail.com>
2015-07-14 16:30:54 +0300

    remove conflict

---
+++ sites/all/modules/custom/icer_main/user_button_top.tpl.php
@@ -0,0 +1,19 @@
+<div class="user-button-item">
+	<a href="<?php print $user_login_link; ?>"><?php print $user_login_image; ?>
+</div>
+
+<div class="user-button-item">
+	<a href="<?php print $profil_link; ?>"><?php print $profil_image; ?>
+</div>
+
+<div class="user-button-item">
+	<a href="<?php print $message_link; ?>"><?php print $message_image; ?>
+</div>
+
+<div class="user-button-item">
+	<a href="<?php print $checkout_link; ?>"><?php print $checkout_image; ?>
+</div>
+
+<div class="user-button-item">
+	<a href="<?php print $add_skils_link; ?>"><?php print $add_skils_image; ?>
+</div>
